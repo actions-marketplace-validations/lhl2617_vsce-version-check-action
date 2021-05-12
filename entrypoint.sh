769146b0874a4f09613641f746d2fe8adec9a8f6
lhl2617 <lhlee888@hotmail.com>
2021-05-12 18:14:15 +0100

    Make indentation 2-spaced

--- entrypoint.sh
+++ entrypoint.sh
@@ -10,27 +10,24 @@ error_not_greater=11
 
 marketplace_version=$(npx vsce show "$extension_publisher.$extension_name" --json | jq -j '.versions[0].version')
 
-if [ -z "$marketplace_version" ]
-then
-    # Unpublished, npx vsce show will still return a 0 exit code
-    echo "Extension is unpublished"
-    if [ "$fail_if_unpublished" = "false" ]
-    then
-        echo "Success: fail-if-unpublished is set to false"
-        exit 0
-    else
-        echo "Failed: fail-if-unpublished is set to true"
-        exit $error_unpublished
-    fi
+if [ -z "$marketplace_version" ]; then
+  # Unpublished, npx vsce show will still return a 0 exit code
+  echo "Extension is unpublished"
+  if [ "$fail_if_unpublished" = "false" ]; then
+    echo "Success: fail-if-unpublished is set to false"
+    exit 0
+  else
+    echo "Failed: fail-if-unpublished is set to true"
+    exit $error_unpublished
+  fi
 else
-    dpkg --compare-versions "$extension_version" "gt" "$marketplace_version"
-    ret_code=$?
-    if [ "$ret_code" = "0" ]
-    then
-        echo "Success: Extension version ($extension_version) is greater than marketplace version ($marketplace_version)"
-        exit 0
-    else
-        echo "Failed: Extension version ($extension_version) is not greater than marketplace version ($marketplace_version)"
-        exit $error_not_greater
-    fi
+  dpkg --compare-versions "$extension_version" "gt" "$marketplace_version"
+  ret_code=$?
+  if [ "$ret_code" = "0" ]; then
+    echo "Success: Extension version ($extension_version) is greater than marketplace version ($marketplace_version)"
+    exit 0
+  else
+    echo "Failed: Extension version ($extension_version) is not greater than marketplace version ($marketplace_version)"
+    exit $error_not_greater
+  fi
 fi
