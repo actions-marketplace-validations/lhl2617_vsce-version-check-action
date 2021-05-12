eba52c754957de4006ac36a4c90392ce1a3a022f
lhl2617 <lhlee888@hotmail.com>
2021-05-12 17:27:02 +0100

    Improve test script

--- test.sh
+++ test.sh
@@ -1,24 +1,32 @@
 #!/bin/bash
 
-set -e
-
 # Ensure we're in the project root
-cd "$(dirname "$0")"
+cd "$(dirname "$0")" || exit 1
+
+extension_publisher=lhl2617
+extension_name=lilypond-formatter
+
+current_version=$(npx vsce show "$extension_publisher.$extension_name" --json | \
+    jq -j '.versions[0].version')
 
 echo "=== Valid ==="
-./entrypoint.sh lhl2617 lilypond-formatter 100000.0.0 false \
-    || (echo "failed" && exit 1) && echo "passed"
+./entrypoint.sh $extension_publisher lilypond-formatter 100000.0.0 false \
+    || exit 1
 
 echo "=== Too old ==="
-./entrypoint.sh lhl2617 lilypond-formatter 0.1.0 false \
-    && (echo "failed" && exit 1) || echo "passed"
+./entrypoint.sh $extension_publisher lilypond-formatter 0.1.0 false \
+    && exit 1
 
-echo "=== Does not exist, OK ==="
-./entrypoint.sh lhl2617 REEEEEEEE 100000.0.0 false \
-    || (echo "failed" && exit 1) && echo "passed"
+echo "=== Equal ==="
+./entrypoint.sh $extension_publisher lilypond-formatter "$current_version" false \
+    && exit 1
 
-echo "=== Does not exist, failed ==="
-./entrypoint.sh lhl2617 REEEEEEEE 100000.0.0 false \
-    && (echo "failed" && exit 1) || echo "passed"
+echo "=== Does not exist, fail-if-unpublished is set to true ==="
+./entrypoint.sh $extension_publisher REEEEEEEE 100000.0.0 true \
+    || exit 1
+
+echo "=== Does not exist, fail-if-unpublished is set to false ==="
+./entrypoint.sh $extension_publisher REEEEEEEE 100000.0.0 false \
+    && exit 1
 
 echo "Tests passed"
